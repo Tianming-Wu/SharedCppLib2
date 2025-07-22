f70547ec779e2b4feecaa3ac1cf02d83c2c8cc10
Tianming Wu <laffeyFeb@outlook.com>
2025-07-22 22:37:10 +0800

    Add new dbgtools.hpp for debug purposes.

---
+++ include/dbgtools.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "stringlist.hpp"
+#include "ansiio.hpp"
+
+namespace dbgtools {
+
+inline std::string strlist_dbgjoin_colored(const std::stringlist &sl) {
+    std::string result; bool st = false;
+    for(const std::string& str : sl) {
+        result += std::color(st?std::tWhite:std::tGreen) + str + std::clearcolor;
+        st = !st;
+    }
+    return result;
+}
+
+} // namespace dbgtools
