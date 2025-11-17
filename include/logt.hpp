a772a159e81923232f6e890214c6fe5589b507fc
Tianming Wu <laffeyFeb@outlook.com>
2025-11-17 16:40:14 +0800

    add basic channel features, not working yet.

--- include/logt.hpp
+++ include/logt.hpp
@@ -22,6 +22,13 @@ Doesn't work well in file mode, disable by yourself.
 #include <chrono>
 #include <functional>
 #include <filesystem>
+#include <bitset>
+#include <map>
+#include <initializer_list>
+
+#ifdef UNICODE
+    #define LOGT_WCHAR_SUPPORT
+#endif
 
 #ifdef LOGT_WCHAR_SUPPORT
     #include <locale>
@@ -29,8 +36,12 @@ Doesn't work well in file mode, disable by yourself.
     #include <type_traits>
 #endif
 
+#ifndef LOGT_MAX_CHANNEL
+    #define LOGT_MAX_CHANNEL 16
+#endif
+
 enum class LogLevel {
-    l_QUIET = -1, // For not logging anything
+    l_QUIET = -1, // For not logging anything (filter only)
     l_DEBUG =  0,
     l_INFO  =  1,
     l_WARN  =  2,
@@ -38,18 +49,47 @@ enum class LogLevel {
     l_FATAL =  4
 };
 
+struct logt_channel {
+    enum class ChannelType {
+        invalid = -1, stdcout = 0, file = 1, custom_ostream = 2
+    };
+
+    logt_channel() :type(ChannelType::invalid), enabled(false) {}
+    inline ~logt_channel() { close(); }
+
+    ChannelType type;
+    bool valid;
+    bool enabled;
+
+    union {
+        std::ofstream fileobj;
+        std::ostream* ostream;
+    };
+
+    void open(); // reserved
+    void close(); // close the stream and set it to invalid
+};
+
+struct logt_channelinfo {
+    std::bitset<LOGT_MAX_CHANNEL> channels;
+    inline bool operator[](size_t index) const { return bool(channels[index]); }
+
+    inline bool stdoutput() const { return bool(channels[0]); }
+};
+
 struct logt_message {
     LogLevel level;
+    logt_channelinfo channels;
     std::chrono::system_clock::time_point timestamp;
     std::string content;
     
     logt_message() = default;
-    logt_message(std::string msg, LogLevel level);
+    logt_message(std::string msg, LogLevel level, logt_channelinfo channels);
 };
 
 class logt_eventbus {
 public:
-    static void push(const std::string& s, LogLevel level);
+    static void push(const std::string& s, LogLevel level, logt_channelinfo channel);
     static bool pop(logt_message& result);
     static void stop();
 
@@ -61,12 +101,46 @@ private:
 };
 
 
+class logt_format {
+public:
+    logt_format() {}
+
+    // logt_format(const logt_format&) = delete;
+    // logt_format& operator=(const logt_format&) = delete;
+    
+    logt_format(const logt_format&) = default;
+    logt_format& operator=(const logt_format&) = default;
+    logt_format(logt_format&&) = default;
+    logt_format& operator=(logt_format&&) = default;
+
+    struct formatSettings {
+        bool enableTimestamp = true;
+        bool enableThreadTag = true;
+        bool enableLocalTag = true;
+    };
+
+    struct formatInfo {
+        LogLevel level;
+        std::string signature;
+    };
+
+    typedef std::function<std::string(const formatSettings&, const formatInfo&)> format_func;
+
+    template<typename T>
+    static std::string format_var(const std::string& format, const T& value) {
+        return std::vformat(format, std::make_format_args(value));
+    }
+
+    format_func formatFunc = nullptr;
+    formatSettings settings;
+};
+
 
 typedef std::function<bool(logt_message&)> preprocessor_t;
 
 class logt_sso {
 public:
-    logt_sso(LogLevel level, const std::string& signature = "");
+    logt_sso(LogLevel level, const logt_format& formatter, const logt_channelinfo& channels, const std::string& signature = "");
     ~logt_sso();
     
     // 禁止拷贝
@@ -108,36 +182,46 @@ public:
 #endif
     
 private:
+    std::string default_formatter(const logt_format::formatSettings& settings, const logt_format::formatInfo& info);
+
+private:
     std::stringstream ss_;
     LogLevel level_;
+    logt_channelinfo channels;
 };
 
 
 class logt_sig {
 public:
-    logt_sig(const std::string& name) : name_(name) {}
+    logt_sig(const std::string& name);
     
     const std::string& name() const { return name_; }
 
-    logt_sso info() const { return logt_sso(LogLevel::l_INFO, name_); }
-    logt_sso warn() const { return logt_sso(LogLevel::l_WARN, name_); }
-    logt_sso error() const { return logt_sso(LogLevel::l_ERROR, name_); }
-    logt_sso fatal() const { return logt_sso(LogLevel::l_FATAL, name_); }
-    logt_sso debug() const { return logt_sso(LogLevel::l_DEBUG, name_); }
+    logt_sso info()  const { return logt_sso(LogLevel::l_INFO,  formatter, channels, name_); }
+    logt_sso warn()  const { return logt_sso(LogLevel::l_WARN,  formatter, channels, name_); }
+    logt_sso error() const { return logt_sso(LogLevel::l_ERROR, formatter, channels, name_); }
+    logt_sso fatal() const { return logt_sso(LogLevel::l_FATAL, formatter, channels, name_); }
+    logt_sso debug() const { return logt_sso(LogLevel::l_DEBUG, formatter, channels, name_); }
+
+    void setFormatter(logt_format::format_func formatter_);
+    void setChannel(int channelid, bool enable);
+    void setChannels(std::initializer_list<int> enabled_channels);
     
 private:
     std::string name_;
+    logt_format formatter;
+    logt_channelinfo channels;
 };
 
 class logt {
     friend class logt_sso;
+    friend class logt_sig;
 public:
     // 静态配置方法
-    static void file(const std::filesystem::path& filename);
-    static void setostream(std::ostream& os);
-    static void stdcout();
+    static int addfile(const std::filesystem::path& filename);
+    static int addostream(std::ostream& os);
+    static void stdcout(bool enable);
     static void claim(const std::string& name);
-    static void closefile();
     inline static void setFilterLevel(LogLevel level) { filter_level_ = level; }
 
     // 静态关闭方法
@@ -151,6 +235,8 @@ public:
      * @param preprocessor A function that takes a logt_message reference and returns a bool.
      */
     static void install_preprocessor(preprocessor_t preprocessor);
+
+    void setFormatter(logt_format::format_func formatter);
 
     inline static void enableSuperTimestamp(bool enabled) { 
         super_timestamp_enabled_ = enabled; 
@@ -171,9 +257,12 @@ private:
     // 静态成员
     static LogLevel filter_level_;
 
-    static std::ofstream log_file_;
-    static std::ostream* output_stream_;
-    static bool use_file_;
+    // static std::ofstream log_file_;
+    // static std::ostream* output_stream_;
+    static logt_channel channels_[LOGT_MAX_CHANNEL];
+    static logt_channelinfo channelinfo_;
+    static int last_channel_id;
+    // static bool use_file_;
 
     static preprocessor_t preprocessor_;
     
@@ -187,6 +276,8 @@ private:
     static std::atomic<bool> super_timestamp_enabled_;
 
     static const char* level_labels_[];
+
+    static logt_format formatter_;
 };
 
 
--- src/logt.cpp
+++ src/logt.cpp
@@ -1,5 +1,7 @@
 #include "logt.hpp"
 
+#include "basics.hpp"
+
 // 静态成员定义
 std::mutex logt_eventbus::mutex_;
 std::condition_variable logt_eventbus::cond_;
@@ -9,9 +11,11 @@ std::atomic<bool> logt_eventbus::stopped_{false};
 
 // 静态成员定义
 LogLevel logt::filter_level_ = LogLevel::l_INFO;
-std::ofstream logt::log_file_;
-std::ostream* logt::output_stream_ = &std::cout;
-bool logt::use_file_ = false;
+// std::ofstream logt::log_file_;
+// std::ostream* logt::output_stream_ = &std::cout;
+logt_channel logt::channels_[LOGT_MAX_CHANNEL];
+int logt::last_channel_id = 1;
+// bool logt::use_file_ = false;
 preprocessor_t logt::preprocessor_;
 std::mutex logt::file_mutex_;
 std::mutex logt::thread_mutex_;
@@ -19,6 +23,8 @@ std::unordered_map<std::thread::id, std::string> logt::thread_names_;
 std::thread logt::worker_;
 std::once_flag logt::worker_flag_;
 std::atomic<bool> logt::super_timestamp_enabled_{false};
+logt_format logt::formatter_;
+logt_channelinfo logt::channelinfo_;
 
 const char* logt::level_labels_[] = {
     "[DEBUG]",
@@ -32,15 +38,40 @@ const char* logt::level_labels_[] = {
 
 // ------ function bodies ------
 
-logt_message::logt_message(std::string msg, LogLevel level) 
+
+void logt_channel::open() {
+    switch(type) {
+        case ChannelType::stdcout:
+            break;
+        case ChannelType::file:
+            break;
+    }
+}
+
+void logt_channel::close() {
+    if(valid) {
+        switch(type) {
+        case ChannelType::stdcout:
+            break;
+        case ChannelType::file:
+            fileobj.close();
+            break;
+        }
+        valid = false;
+    }
+}
+
+
+logt_message::logt_message(std::string msg, LogLevel level, logt_channelinfo channels) 
 : timestamp(std::chrono::system_clock::now())
 , content(std::move(msg))
-, level(level) {}
+, level(level)
+, channels(channels) {}
 
 
-void logt_eventbus::push(const std::string& s, LogLevel level) {
+void logt_eventbus::push(const std::string& s, LogLevel level, logt_channelinfo channel) {
     std::unique_lock<std::mutex> lock(mutex_);
-    queue_.push(logt_message(s, level));
+    queue_.push(logt_message(s, level, channel));
     cond_.notify_one();
 }
 
@@ -64,54 +95,112 @@ void logt_eventbus::stop() {
 
 
 
-logt_sso::logt_sso(LogLevel level, const std::string& signature) 
-    : level_(level)
+logt_sso::logt_sso(LogLevel level, const logt_format& formatter, const logt_channelinfo& channels, const std::string& signature) 
+    : level_(level), channels(channels)
 {
     logt::ensure_worker_started();
 
-    // 添加级别标签
-    ss_ << logt::level_labels_[static_cast<int>(level)] << " ";
-    
-    // 添加线程标识
-    auto thread_name = logt::get_thread_name();
-    if (!thread_name.empty()) {
-        ss_ << "[" << thread_name << "] ";
+    logt_format::formatInfo info;
+    info.level = level;
+    info.signature = signature;
+
+    if(formatter.formatFunc) {
+        formatter.formatFunc(formatter.settings, info);
     } else {
-        ss_ << "[" << std::this_thread::get_id() << "] ";
-    }
-    
-    // 添加签名（如果有）
-    if (!signature.empty()) {
-        ss_ << "[" << signature << "] ";
+        default_formatter(formatter.settings, info);
     }
 }
 
 logt_sso::~logt_sso() {
     // 析构时自动推送完整日志
     if (level_ >= logt::filter_level_) {
-        logt_eventbus::push(ss_.str(), level_);
+        logt_eventbus::push(ss_.str(), level_, channels);
     }
 }
 
+std::string logt_sso::default_formatter(const logt_format::formatSettings& settings, const logt_format::formatInfo &info)
+{
+    std::string format_result;
+    format_result = logt::level_labels_[static_cast<int>(info.level)];
 
+    if(settings.enableThreadTag) {
+        format_result += std::string(" ");
+        auto thread_name = logt::get_thread_name();
+        if (!thread_name.empty()) {
+            format_result += "[" + thread_name + "] ";
+        } else {
+            format_result += "[" + streamed_to_string(std::this_thread::get_id()) + "] ";
+        }
+    }
 
-void logt::file(const std::filesystem::path& filename) {
-    ensure_worker_started();
-    std::unique_lock<std::mutex> lock(file_mutex_);
-    if (log_file_.is_open()) log_file_.close();
-    log_file_.open(filename, std::ios::app);
-    use_file_ = log_file_.is_open();
+    if (!info.signature.empty()) {
+        ss_ << "[" << info.signature << "] ";
+    }
+
+    return format_result;
 }
 
-void logt::setostream(std::ostream& os) {
-    ensure_worker_started();
-    std::unique_lock<std::mutex> lock(file_mutex_);
-    output_stream_ = &os;
-    use_file_ = false;
+
+
+logt_sig::logt_sig(const std::string& name)
+    : name_(name)
+{
+    formatter = logt::formatter_;
 }
 
-void logt::stdcout() {
-    setostream(std::cout);
+void logt_sig::setFormatter(logt_format::format_func formatter_) {
+    formatter.formatFunc = formatter_;
+}
+
+void logt_sig::setChannel(int channelid, bool enable) {
+    channels.channels[channelid] = enable;
+}
+
+void logt_sig::setChannels(std::initializer_list<int> enabled_channels) {
+    std::bitset<LOGT_MAX_CHANNEL> newbs(0);
+
+    for(int i : enabled_channels) {
+        newbs[i] = true;
+    }
+
+    channels.channels = newbs; // overwrite
+}
+
+
+
+int logt::addfile(const std::filesystem::path& filename) {
+    ensure_worker_started();
+    std::unique_lock<std::mutex> lock(file_mutex_);
+
+    int newid = last_channel_id++;
+    logt_channel &channel = channels_[newid];
+
+    channel.type = logt_channel::ChannelType::file;
+    channel.fileobj = std::ofstream(filename, std::ios::app);
+    channel.valid = channel.fileobj.is_open();
+    channel.enabled = true;
+
+    return newid;
+}
+
+int logt::addostream(std::ostream& os) {
+    ensure_worker_started();
+    std::unique_lock<std::mutex> lock(file_mutex_);
+
+    int newid = last_channel_id++;
+    logt_channel &channel = channels_[newid];
+
+    channel.type = logt_channel::ChannelType::custom_ostream;
+    channel.ostream = &os;
+    channel.valid = (&os != nullptr); // may be useless (
+    channel.enabled = true;
+
+    return newid;
+}
+
+void logt::stdcout(bool enable) {
+    ensure_worker_started();
+    channels_[0].enabled = true;
 }
 
 void logt::claim(const std::string& name) {
@@ -120,21 +209,14 @@ void logt::claim(const std::string& name) {
     thread_names_[std::this_thread::get_id()] = name;
 }
 
-void logt::closefile() {
-    std::unique_lock<std::mutex> lock(file_mutex_);
-    if (log_file_.is_open()) log_file_.close();
-    use_file_ = false;
-    output_stream_ = &std::cout; // 回退到标准输出
-}
-
 void logt::shutdown() {
     logt_eventbus::stop();
     if (worker_.joinable()) worker_.join();
 
     std::unique_lock<std::mutex> lock(file_mutex_);
-    if (log_file_.is_open()) {
-        log_file_.close();
-    }
+    // for(logt_channel& ch : channels_) {
+    //     if(channels_.type == logt_channel::ChannelType::stdcout)
+    // }
 }
 
 void logt::exit(int exitcode)
@@ -148,6 +230,10 @@ void logt::install_preprocessor(preprocessor_t preprocessor)
     preprocessor_ = std::move(preprocessor);
 }
 
+void logt::setFormatter(logt_format::format_func formatter)
+{
+    formatter_.formatFunc = formatter;
+}
 
 std::string logt::get_thread_name() {
     std::unique_lock<std::mutex> lock(thread_mutex_);
@@ -156,6 +242,12 @@ std::string logt::get_thread_name() {
 }
 
 void logt::worker_thread() {
+    // make sure stdcout is configured.
+    channels_[0].type = logt_channel::ChannelType::stdcout;
+    channels_[0].ostream = &std::cout;
+    channels_[0].valid = true;
+    channels_[0].enabled = true;
+
     logt_message message;
     while (logt_eventbus::pop(message)) {
         if(preprocessor_) {
@@ -168,12 +260,25 @@ void logt::worker_thread() {
 
 void logt::write_message(const logt_message& message) {
     std::unique_lock<std::mutex> lock(file_mutex_);
-    std::string timestamp_str = format_timestamp(message.timestamp);
-    if (use_file_ && log_file_.is_open()) {
-        log_file_ << timestamp_str << " " << message.content << std::endl;
-        log_file_.flush();
-    } else if (output_stream_) {
-        *output_stream_ << timestamp_str << " " << message.content << std::endl;
+    std::string timestamp_str = (formatter_.settings.enableTimestamp)?(format_timestamp(message.timestamp) + " "):"";
+
+    if(message.channels.stdoutput()) {
+        *channels_[0].ostream << timestamp_str << message.content << std::endl;
+    }
+
+    for(int i = 1; i < LOGT_MAX_CHANNEL; i++) {
+        if(message.channels[i] && channels_[i].valid) {
+            switch(channels_[i].type) {
+            case logt_channel::ChannelType::file:
+                channels_[i].fileobj << timestamp_str << message.content << std::endl;
+                channels_[i].fileobj.flush();
+                break;
+            case logt_channel::ChannelType::custom_ostream:
+                *channels_[0].ostream << timestamp_str << message.content << std::endl;
+                // endl already contains flush
+                break;
+            }
+        }
     }
 }
 
@@ -204,7 +309,7 @@ std::string logt::format_timestamp(const std::chrono::system_clock::time_point& 
         auto seconds = std::chrono::duration_cast<std::chrono::seconds>(since_epoch);
         auto micros = std::chrono::duration_cast<std::chrono::microseconds>(since_epoch - seconds);
         
-        int milliseconds = micros.count() / 1000;
+        int milliseconds = static_cast<int>(micros.count() / 1000);
         int microseconds = micros.count() % 1000;
         
         return std::format("[{:04d}/{:02d}/{:02d} {:02d}:{:02d}:{:02d}.{:03d}.{:03d}]",
