eafa35af0beb6d93987f8c88696d25d1cdb3041d
Tianming Wu <laffeyFeb@outlook.com>
2026-03-04 16:22:24 +0800

    Added an string style input for bytearray encoding.

--- include/Base64.hpp
+++ include/Base64.hpp
@@ -16,6 +16,14 @@ namespace Base64 {
 std::string encode(const std::bytearray& input);
 std::bytearray decode(const std::string& input);
 
+// This is using bytearray's raw, no-terminator constructor.
+// If you want custom behavior, you should construct bytearray
+// yourself and call the above functions directly.
+// This one is fine with most use cases.
+inline std::string encode(const std::string& input) {
+    return encode(std::bytearray(input));
+}
+
 // std::string encode(unsigned char *input , size_t input_len);
 // std::string decode(std::string input);
 
