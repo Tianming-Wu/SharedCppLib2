0ae5f5ae4d96f338707d8299665fb81778de12c3
Tianming Wu <laffeyFeb@outlook.com>
2026-03-04 16:23:16 +0800

    Added scl-typemask ban macro

--- include/typemask.hpp
+++ include/typemask.hpp
@@ -1,7 +1,14 @@
 #pragma once
 
+/*
+    Type mask for some type definitions, and make them compatible with WinAPI types,
+    while also not forcing users to include Windows headers if they don't want to.
+*/
+
 #ifndef SCL_TYPEMASK_HPP
 #define SCL_TYPEMASK_HPP
+
+#ifndef SCL_NO_TYPEMASK
 
 typedef unsigned char byte_t, *pbyte_t;
 typedef unsigned short word_t, *pword_t;
@@ -10,5 +17,5 @@ typedef unsigned long long qword_t, *pqword_t;
 
 typedef void* ptr_t;
 
-
-#endif
+#endif // SCL_NO_TYPEMASK
+#endif // SCL_TYPEMASK_HPP
